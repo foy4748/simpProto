120c451debaf4f73a834702a33a8bec1faeefc3a
foy4748 <faisaljfcl@gmail.com>
2021-05-16 13:47:29 +0600

    Working fine for multiple arguments
    Catching multiple arguments using *arg in simps.py.
    Requires further documentation for usability.

--- simp.py
+++ simp.py
@@ -1,11 +1,31 @@
 from simps import simp
 
+import math
+import numpy as np
+
+
+def F(phi, args):
+    thetaM = args[0]
+    A = math.sin(thetaM/2.00)*math.sin(thetaM/2.00)
+    B = math.sin(phi)*math.sin(phi)
+    C = 1.000 - A*B
+    D = 1.000/C
+    final = math.sqrt(D)
+    return final
+'''
 def F(x):
-    return x*x*x;
+    return x*x*x
     
 
 #             Lower Limit,    Upper Limit, Number of Iterations, Functions
-result = simp(2, 3, 100, F).result13()  #result13 for Simpson's 1/3 rule
-
+result = simp(2, 3, 120, F).result38()  #result13 for Simpson's 1/3 rule
 print(result)
+'''
 
+
+phi = np.linspace(0, math.pi/2.00, 1001)
+
+a = 4.0*math.sqrt(0.0253)
+for p in phi:
+    result = simp(0, math.pi/2.00, 120, F, p).result38()  #result13 for Simpson's 1/3 rule
+    print("{}\t{}".format(p, a*result))
--- simps.py
+++ simps.py
@@ -1,10 +1,14 @@
 class simp:
-    def __init__(self, low, high, steps, func):
+    def __init__(self, low, high, steps, func, *args):
         self.lowerLimit = low ; self.upperLimit = high 
         self.nOS = steps
         self.func = func
-        
         self.step_size = (high - low)/steps
+        try:
+            self.args = args
+        except:
+            pass
+        
 
     def result13(self):
         I = 0
@@ -12,30 +16,54 @@ class simp:
         func = self.func
         A = self.lowerLimit
         B = self.upperLimit
-
-        for i in range(1, self.nOS, 2):
-            I += func(self.lowerLimit + i*self.step_size)
         
-        for i in range(2,self.nOS,2):
-            J += func(self.lowerLimit + i*self.step_size)
+        try:
+            for i in range(1, self.nOS, 2):
+                I += func(self.lowerLimit + i*self.step_size, self.args)
+            
+            for i in range(2,self.nOS,2):
+                J += func(self.lowerLimit + i*self.step_size, self.args)
+            
+            result = self.step_size*(func(A,self.args) + 4e0*I + 2e0*J + func(B,self.args))/3e0
+        except:
+            for i in range(1, self.nOS, 2):
+                I += func(self.lowerLimit + i*self.step_size)
+            
+            for i in range(2,self.nOS,2):
+                J += func(self.lowerLimit + i*self.step_size)
+            
+            result = self.step_size*(func(A) + 4e0*I + 2e0*J + func(B))/3e0
         
-        return self.step_size*(func(A) + 4e0*I + 2e0*J + func(B))/3e0
+        return result
     
     def result38(self):
         I = 0
         J = 0
         K = 0
+        func = self.func
         A = self.lowerLimit
         B = self.upperLimit
-        func = self.func
         
-        for i in range(1, self.nOS, 3):
-            I += func(self.lowerLimit + i*self.step_size)
+        try:
+            for i in range(1, self.nOS, 3):
+                I += func(self.lowerLimit + i*self.step_size, self.args)
+            
+            for i in range(2,self.nOS,3):
+                J += func(self.lowerLimit + i*self.step_size, self.args)
+            
+            for i in range(3,self.nOS,3):
+                K += func(self.lowerLimit + i*self.step_size, self.args)
+            result = 3e0*self.step_size*(func(A,self.args) + 3e0*I + 3e0*J + 2e0*K + func(B,self.args))/8e0
+        except:
+            for i in range(1, self.nOS, 3):
+                I += func(self.lowerLimit + i*self.step_size)
+            
+            for i in range(2,self.nOS,3):
+                J += func(self.lowerLimit + i*self.step_size)
+            
+            for i in range(3,self.nOS,3):
+                K += func(self.lowerLimit + i*self.step_size)
+            result = 3e0*self.step_size*(func(A) + 3e0*I + 3e0*J + 2e0*K + func(B))/8e0
         
-        for i in range(2,self.nOS,3):
-            J += func(self.lowerLimit + i*self.step_size)
-            
-        for i in range(2,self.nOS,3):
-            K += func(self.lowerLimit + i*self.step_size)
-            
-        return 3e0*self.step_size*(func(A) + 3e0*I + 3e0*J + 2e0*K + func(B))/8e0
+        return result
+
